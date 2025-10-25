ef07dcd616e452979e78bee380ecc2c55c2e0266
mrunalg25 <golimrunal25@gmail.com>
2025-10-25 19:01:04 -0400

    redux

--- app/Labs/Lab4/page.tsx
+++ app/Labs/Lab4/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
@@ -10,8 +11,14 @@ import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
 import ParentStateComponent from "./ParentStateComponent";
-import ReduxExamples from "./ReduxExamples";
 import Link from "next/link";
+
+// Dynamically import Redux components to avoid SSR issues
+import dynamic from 'next/dynamic';
+
+const ReduxExamples = dynamic(() => import("./ReduxExamples"), {
+  ssr: false,
+});
 
 export default function Lab4() {
   function sayHello() {
@@ -41,3 +48,48 @@ export default function Lab4() {
     </div>
   );
 }
+
+
+// "use client";
+// import ClickEvent from "./ClickEvent";
+// import PassingDataOnEvent from "./PassingDataOnEvent";
+// import PassingFunctions from "./PassingFunctions";
+// import EventObject from "./EventObject";
+// import Counter from "./Counter";
+// import BooleanStateVariables from "./BooleanStateVariables";
+// import StringStateVariables from "./StringStateVariables";
+// import DateStateVariable from "./DateStateVariable";
+// import ObjectStateVariable from "./ObjectStateVariable";
+// import ArrayStateVariable from "./ArrayStateVariable";
+// import ParentStateComponent from "./ParentStateComponent";
+// import ReduxExamples from "./ReduxExamples";
+// import Link from "next/link";
+
+// export default function Lab4() {
+//   function sayHello() {
+//     alert("Hello");
+//   }
+  
+//   return (
+//     <div id="wd-lab4" className="container">
+//       <h2>Lab 4</h2>
+//       <ClickEvent />
+//       <PassingDataOnEvent />
+//       <PassingFunctions theFunction={sayHello} />
+//       <EventObject />
+//       <Counter />
+//       <BooleanStateVariables />
+//       <StringStateVariables />
+//       <DateStateVariable />
+//       <ObjectStateVariable />
+//       <ArrayStateVariable />
+//       <ParentStateComponent />
+//       <ReduxExamples />
+      
+//       <hr />
+//       <Link href="/Labs">
+//         <button className="btn btn-secondary">← Back to Labs</button>
+//       </Link>
+//     </div>
+//   );
+// }
